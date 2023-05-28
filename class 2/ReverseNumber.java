9944a00edc4245e150e515afb5f0fd51a753aab1
simer <simerpreet2844@gmail.com>
2023-05-28 22:51:39 +0530

    reverse array program

--- class 2/ReverseNumber.java
+++ class 2/ReverseNumber.java
@@ -1,16 +1,31 @@
-/**
- * ReverseNumber
- */
+
+// solving this question using brute force technique
+
 public class ReverseNumber {
 
-     public static void main(String[] args) {
-        
-        // reverse given number of an array;
-        
-        int arr[] = {1,2,3,4,5,6,6,7,8,9,10};
+    public static void main(String[] args) {
 
-        for(int i=arr.length-1; i >=0; i--){
-            System.out.print(arr[i] + " ");
+        // reverse given number of an array;
+
+        int arr[] = { 1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10 };
+
+        // for(int i=arr.length-1; i >=0; i--){
+        // System.out.print(arr[i] + " ");
+        // }
+
+        // make a new array
+        // and reverse the looping
+        // and store into it.
+
+        int indexArr2 = 0;
+        int arr2[] = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            arr2[indexArr2] = arr[i];
+            indexArr2++;
         }
-     }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+    }
 }
